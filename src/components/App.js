82b4687986f8d17ce618defdf8cb9d69044f39c5
Shumpulp <samuelmwaiki@gmail.com>
2025-04-11 14:51:02 +0300

    pr 1

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,25 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+
+function About () {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>This is the about page of our application.</p>
+    </div>
+  );
+};
+
+
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+     <Home/>
+      <About/>
     </div>
   );
 }
